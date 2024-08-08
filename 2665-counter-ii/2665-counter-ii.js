fdfd142c0ec4ad727909219da4ee381625052968
yesolz <112631986+yesolz@users.noreply.github.com>
2024-08-08 22:41:07 +0900

    Time: 68 ms (13.42%), Space: 51.6 MB (57.01%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var resetValue = init
+    return {
+        increment: function(){
+            return ++init;
+        },
+        decrement: function(){
+            return --init;
+        },
+        reset: function(){
+            init = resetValue
+            return init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
