a411e8bc5531ab099ffc1d55c2676915edc3cb6d
yesolz <112631986+yesolz@users.noreply.github.com>
2024-08-14 23:21:00 +0900

    Time: 50 ms (89.16%), Space: 50.1 MB (95.21%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let isCalled = false
+    return function (...args) {
+        if (!isCalled) {
+            isCalled = true
+            return fn(...args)
+        }
+        return undefined
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
