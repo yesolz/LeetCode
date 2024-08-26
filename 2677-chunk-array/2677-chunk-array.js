30a7bb8a9b54f5e10064d93a6807e5814ec694b3
yesolz <112631986+yesolz@users.noreply.github.com>
2024-08-27 08:16:19 +0900

    Time: 62 ms (24.25%), Space: 51.6 MB (59.19%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let chunked_arr = []
+    for (let i=0; i<arr.length; i+=size){
+        let chunk = []
+        for (let j=i; j<size+i; j++){
+            if (arr[j] != null){
+                chunk.push(arr[j])
+            }
+        }
+        chunked_arr.push(chunk)
+    }
+    return chunked_arr;
+};
