c137ab326aacce549fbfdcb45566198be3ecd55d
yesolz <112631986+yesolz@users.noreply.github.com>
2024-08-27 08:36:15 +0900

    Time: 55 ms (59.49%), Space: 52 MB (27.29%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -6,10 +6,7 @@
 var chunk = function(arr, size) {
     let chunked_arr = []
     for (let i=0; i<arr.length; i+=size){
-        let chunk = []
-        for (let j=i; j<size+i && j<arr.length; j++){
-            chunk.push(arr[j])
-        }
+        let chunk = arr.slice(i, i+size)
         chunked_arr.push(chunk)
     }
     return chunked_arr;
