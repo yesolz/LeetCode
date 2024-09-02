dfb50057d45b7c7093ab6a9217832ea5ab8566f1
yesolz <112631986+yesolz@users.noreply.github.com>
2024-09-02 12:34:32 +0900

    Time: 122 ms (63.24%), Space: 68.1 MB (88.08%) - LeetHub

--- 2631-group-by/2631-group-by.js
+++ 2631-group-by/2631-group-by.js
@@ -19,13 +19,3 @@ Array.prototype.groupBy = function(fn) {
     return grouped
 
 };
-
-
-
-/**
- * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
- */
-
-
-// fn: 함수. 기준이 됨. 
-// fn에 넣었을 때의 결과에 따라 group으로 묶음.
