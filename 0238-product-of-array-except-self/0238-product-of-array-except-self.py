c726049267ee3f2d6c08e6bd29b2299940887da5
yesolz <112631986+yesolz@users.noreply.github.com>
2024-11-08 17:09:17 +0900

    Time: 35 ms (26.55%), Space: 26.1 MB (28.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,26 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
 
-        # 왼쪽 곱과 오른쪽 곱의 누적 계산!
-        left = [1] * len(nums)
-        for i in range(len(nums)):
+        left = [1] * n
+        for i in range(n):
             if i == 0:
                 left[i] = nums[i]
             else:
                 left[i] = (left[i-1] * nums[i])
 
-        right = [1] * len(nums)
-        for i in range(len(nums)-1, -1, -1):
-            if i == len(nums)-1:
+        right = [1] * n
+        for i in range(n-1, -1, -1):
+            if i == n-1:
                 right[i] = nums[i]
             else:
                 right[i] = (right[i+1] * nums[i])
-                
-        answer = [1] * len(nums)
-        for i in range(len(nums)):
+
+        answer = [1] * n
+        for i in range(n):
             if i == 0:
                 answer[i] = right[i+1]
-            elif i == len(nums) - 1:
+            elif i == n - 1:
                 answer[i] = left[i-1]
             else:
                 answer[i] = (left[i-1] * right[i+1])
